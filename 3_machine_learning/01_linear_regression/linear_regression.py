833e59a59aa804868ba57e71471896760c00690f
LuoXianchao <nhjclxc@163.com>
2023-12-07 18:52:16 +0800

    机器学习相关资料-1

---
+++ 3_machine_learning/01_linear_regression/linear_regression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author    : LuoXianchao
+# Datetime  : 2023/12/7 15:45
+# Module    : linear_regression.py
+# explain   :
+
+import numpy
+
+class LinearRegression:
+    pass
